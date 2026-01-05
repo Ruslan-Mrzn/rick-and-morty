2d9566c9c3ee27e50def52f8788b9595f28526c4
Ruslan <russl.murzin@gmail.com>
2026-01-06 00:56:55 +0300

    chore: добавить плагин vite-plugin-svgr и настроить

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true
+      },
+      include: '**/*.svg?react'
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
