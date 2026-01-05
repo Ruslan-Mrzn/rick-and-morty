5cea879dcf31933f55ba48fd07c98b59be218d2c
Ruslan <russl.murzin@gmail.com>
2026-01-05 11:05:53 +0300

    feat: настроить структуру проекта

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import './index.css';
-import App from './components/App';
+import App from './shared/components/App';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
--- src/shared/components/App/index.tsx
+++ src/shared/components/App/index.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router';
-import MainLayout from '../MainLayout';
+import MainLayout from '@/shared/components/MainLayout';
 import { HomePage, CharacterPage } from '@/pages';
 
 const App = () => {
