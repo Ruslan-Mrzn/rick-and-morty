70aac9c6904a15ef7b771d6c6fc34089693e02da
Ruslan <russl.murzin@gmail.com>
2026-04-11 15:30:25 +0300

    fix: исправить замечания по ревью

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import svgr from 'vite-plugin-svgr';
 
+const isVisualizer = process.env.BUILD_VISUALIZER === 'true';
+
+// Read version from package.json
+const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+const version = pkg.version;
+const date = new Date().toISOString().split('T')[0].replace(/-/g, '');
+
 export default defineConfig(() => ({
-  // eslint-disable-next-line no-undef
   base: process.env.VITE_BASE_PATH || '/',
   plugins: [
     react(),
@@ -16,11 +24,20 @@ export default defineConfig(() => ({
       },
       include: '**/*.svg?react'
     }),
-    ViteImageOptimizer()
+    ViteImageOptimizer(),
+    ...(isVisualizer
+      ? [
+          visualizer({
+            open: true,
+            filename: `reports/stats-v${version}-${date}.html`,
+            gzipSize: true,
+            brotliSize: true
+          })
+        ]
+      : [])
   ],
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undef
       '@': path.resolve(__dirname, './src')
     }
   }
