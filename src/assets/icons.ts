1a557563ee3f669ffc979dc612fa85916833f7b0
Ruslan <russl.murzin@gmail.com>
2026-05-14 16:20:50 +0300

    feat: добавить переключение на темную тему

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,6 +1,7 @@
 import LeftArrowIcon from '@/assets/icons/arrow-left-icon.svg?react';
 import CheckIcon from '@/assets/icons/check-icon.svg?react';
 import CrossIcon from '@/assets/icons/cross-icon.svg?react';
+import DarkThemeIcon from '@/assets/icons/dark-theme-icon.svg?react';
 import EditIcon from '@/assets/icons/edit-icon.svg?react';
 import LightThemeIcon from '@/assets/icons/light-theme-icon.svg?react';
 import LogoIcon from '@/assets/icons/logo-icon.svg?react';
@@ -11,6 +12,7 @@ import SuccessLoadedIcon from '@/assets/icons/success-loaded-icon.svg?react';
 export {
   LogoIcon,
   LightThemeIcon,
+  DarkThemeIcon,
   LeftArrowIcon,
   SelectorArrowIcon,
   SearchIcon,
