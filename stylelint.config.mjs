0e825b9ac0b5eff67e1ada4dd992bd7f2dd716bd
Ruslan <russl.murzin@gmail.com>
2026-05-31 16:16:09 +0300

    refactor: централизовать mobile-брейкпоинт

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,5 +3,7 @@ export default {
   extends: [
     'stylelint-prettier/recommended',
     'stylelint-config-clean-order/error'
-  ]
+  ],
+  customSyntax: 'postcss-scss',
+  ignoreFiles: ['dist/**', 'dev-dist/**', 'node_modules/**']
 };
