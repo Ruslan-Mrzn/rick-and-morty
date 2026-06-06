12688c4ff8b8c5bb39e3fe96ebcb857672e82b77
Ruslan <russl.murzin@gmail.com>
2026-06-06 14:35:21 +0300

    feat: добавить story Selector

---
+++ .storybook/preview.tsx
@@ -0,0 +1,17 @@
+import '@sb/i18n';
+import type { Preview } from '@storybook/react-vite';
+
+import '@/styles/global.scss';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i
+      }
+    }
+  }
+};
+
+export default preview;
--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import js from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 import prettier from 'eslint-config-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import storybook from 'eslint-plugin-storybook';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -88,5 +90,6 @@ export default defineConfig([
       globals: globals.node
     }
   },
-  prettier
+  prettier,
+  ...storybook.configs['flat/recommended']
 ]);
